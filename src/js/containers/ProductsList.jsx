6c500250492b9cb844be5b2c2c6dabcb45db342b
Daniel Ruiz <daniel.ruiz@rapidfire.com>
2021-09-28 23:27:22 -0500

    Added logic of ProductsList

--- src/js/containers/ProductsList.jsx
+++ src/js/containers/ProductsList.jsx
@@ -2,18 +2,47 @@ import React, { useEffect } from 'react';
 import { useContext } from 'react';
 import StateContext from '../base/context';
 import axios from 'axios';
+import * as TYPES from '../base/types';
 import Product from '../components/Product';
 import Favorites from '../components/Favorites';
+import AddToCart from '../components/AddToCart';
 
 const ProductsList = () => {
   const [state, dispatch] = useContext(StateContext);
   const {
     shop: { products },
     favorites,
+    cart,
   } = state;
 
-  const handlerAddFavorites = (event) => {
-    console.log('SS', event);
+  const handlerAddFavorites = (productSelected) => {
+    const { productId, productName } = productSelected;
+    const isRepeatingElement = favorites.some((item) => item.id === productId);
+    if (!isRepeatingElement) {
+      dispatch({
+        type: TYPES.ADD_FAVORITES_PRODUCTS,
+        payload: { productId, productName },
+      });
+    } else {
+      dispatch({
+        type: TYPES.DELETE_FAVORITES_PRODUCTS,
+        payload: { productId },
+      });
+    }
+  };
+
+  const handlerIncreaseProducts = (productId) => {
+    dispatch({
+      type: TYPES.INCREASE_TO_CART,
+      payload: { productId },
+    });
+  };
+
+  const handlerDecreaseProducts = (productId) => {
+    dispatch({
+      type: TYPES.DECREASE_TO_CART,
+      payload: { productId },
+    });
   };
 
   const getProducts = async () => {
@@ -22,7 +51,7 @@ const ProductsList = () => {
     );
     const { Comercio, Productos, Categorias } = res.data;
     dispatch({
-      type: 'FETCH_PRODUCTS',
+      type: TYPES.FETCH_PRODUCTS,
       payload: {
         commerce: Comercio,
         products: Productos,
@@ -38,15 +67,29 @@ const ProductsList = () => {
   return (
     <div className='list-group h-100'>
       <h3>Agregados a favoritos {favorites.length}</h3>
+      <h3>Carrito {cart.length}</h3>
+      {console.log(cart)}
       {products &&
         Object.entries(products).map((item, i) => {
-          const { nombreProducto } = item[1];
+          const { nombreProducto, idProducto } = item[1];
+          const isRepeatingElement = favorites.some(
+            (item) => item.id === idProducto
+          );
+          const isAddedToCart = cart.filter((item) => item.id === idProducto);
           return (
             <div key={i} className='d-flex justify-content-between col-4 my-1'>
               <Product productName={nombreProducto} />
               <Favorites
                 productName={nombreProducto}
+                productId={idProducto}
+                isRepeating={isRepeatingElement}
                 addFavorites={(e) => handlerAddFavorites(e)}
+              />
+              <AddToCart
+                cart={isAddedToCart}
+                productId={idProducto}
+                decreaseProducts={(e) => handlerDecreaseProducts(e)}
+                increaseProducts={(e) => handlerIncreaseProducts(e)}
               />
             </div>
           );
